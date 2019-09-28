c8dc946bd534658a956044f178fa1d8e8b73e6f6
sidneyaguirre <sidney.aguirrec@gmail.com>
2019-09-27 20:22:33 -0500

    sumaEnVector correction

--- sumaEnVector.js
+++ sumaEnVector.js
@@ -1,17 +1,20 @@
 let v = [2, 3, 9, 1, 2, 3];
-let number = 13;
+let number = 10;
 
-let aux = 0; 
+let aux = 0;
 let i = 0;
 
 while (i < v.length) {
-    aux += v[i];
-    if((aux + v[i+1]) === number) {
-        return console.log(true);
-    }else {
-        if((aux + v[i+1]) > number){
-            aux = v[i];
-        }
-        i++;
-    };
-};
+  aux += v[i];
+  if (aux === number) {
+    return console.log(true);
+  } else {
+    if (aux + v[i + 1] > number) {
+      aux = v[i];
+    }
+    i++;
+    if (i === v.length) {
+      return console.log(false);
+    }
+  }
+}
